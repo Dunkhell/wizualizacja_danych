903098b019ac1870ce7fec67b12fac9fa85fd09d
Dunkhell <58849959+Dunkhell@users.noreply.github.com>
2020-03-05 14:43:23 +0100

    Add files via upload

---
+++ lekcja2.py
@@ -0,0 +1,65 @@
+#ZADANIE 1
+
+# zdanie= input("Wpisz zdanie: ")
+# a = zdanie.count(" ")
+# print(a)
+
+# import sys
+#zadanie 2
+# print("Podaj dwie wartosci: ")
+# a=sys.stdin.readline()
+# b=sys.stdin.readline()
+# c=int(a)*int(b)
+# c=str(c)
+# sys.stdout.write(c)
+
+#zadanie 4
+# a= int(input("Twoja liczba: "))
+# print(abs(a))
+
+#zadanie 5
+# a= int(input("Wprowadz A: "))
+# b= int(input("Wprowadz B: "))
+# c= int(input("Wprowadz C: "))
+
+# if a>b and b>c and 0 < a <= 10:
+#     print("Warunku sa spelnione! ")
+# else:
+#     print("Warunki nie sa splenione! ")
+
+#zadanie 6
+# for i in range(5,500, 5):
+#     print(i)
+
+#zadanie 7
+# b= int(input("Ile liczb bedziemy potegowac: "))
+
+# for i in range(0,b,1):
+#     a=int(input("wprowadz liczbe: "))
+#     a**=2
+#     print(a)
+
+#zadanie 8
+# b= int(input("ile liczb wpiszemy do listy: "))
+# a=0
+# list=[]
+# for i in range(a,b,1):
+#     c=int(input("Wprowadz liczbe: "))
+#     list.insert(a,c)
+# print(list)
+
+#zadanie 9
+# a= int(input("wprowadz liczbe: "))
+# suma = 0
+# for cyfra in str(a):
+#     suma=suma+int(cyfra)
+# print(suma)
+
+#zadanie 10
+# b=int(input("Podaj wyoskosc wiezy: "))
+# while b>10:
+#     b=int(input("Podaj wyoskosc wiezy: "))
+
+
+# for i in range(1,b+1,1):
+#     print("A"*i)
